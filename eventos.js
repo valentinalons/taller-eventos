ebc086ece57407ef9edb9a7fbc85deb3c6f2e184
Valentina Alonso <alonsovalentina55@gmail.com>
2023-08-12 20:05:42 -0300

    Solucion realizada en rama

--- eventos.js
+++ eventos.js
@@ -1,10 +1,21 @@
 // Obtengo el div por su id
 const divSaludo = document.getElementById('divSaludo');
 
+// Obtengo el boton por su id
+const botSaludo = document.getElementById("botSaludo");
+
 // Defino la función que se ejecutará al hacer clic en el div
 function saludarDiv() {
   alert('Hola! Soy el div');
 }
+
+//Defino la funcion que se ejecutara al hacer click en el boton
+function saludarBot() {
+  alert('Hola!');
+}
+
+// Asocio el manejador de eventos al hacer clic en el boton
+botSaludo.addEventListener(click, saludarBot);
 
 // Asocio el manejador de eventos al hacer clic en el div
 divSaludo.addEventListener('click', saludarDiv);
